5ff68072da9000443f97924b57e1b26ef91d57e6
Junior-Tour <juniortour@qq.com>
2016-12-18 21:44:00 +0800

    2016年12月18日21:43:57

--- 作品/淘宝首页/js/revealIconAndPlaceholder.js
+++ 作品/淘宝首页/js/revealIconAndPlaceholder.js
@@ -4,14 +4,18 @@
 
 var searchInput=document.querySelectorAll("#search-input")[0];
 searchInput.oninput= function () {
-    revealIconAndPlaceholder();
+    //alert(this.value.length);
+    changeStatus("none");
+    if (searchInput.value.length==0) {
+        changeStatus("block");
+    }
 };
 
-function revealIconAndPlaceholder() {
+function changeStatus(displayValue) {
     var searchForm=document.querySelectorAll("#search-form")[0];
     var searchIcon=searchForm.querySelectorAll(".tb-icon")[0];
     var searchPlaceholder=searchForm.querySelectorAll(".search-placeholder")[0];
 
-    searchIcon.style.display="none";
-    searchPlaceholder.style.display="none";
+    searchIcon.style.display=displayValue;
+    searchPlaceholder.style.display=displayValue;
 }
