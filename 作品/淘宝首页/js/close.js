0fad7c4c24abdf558561a074c31ef732f6691c3a
Junior-Tour <juniortour@qq.com>
2017-01-12 20:57:02 +0800

    2017年1月12日20:56:40

--- 作品/淘宝首页/js/close.js
+++ 作品/淘宝首页/js/close.js
@@ -5,15 +5,15 @@ var close_buttons=document.getElementsByClassName("close");
 var i;
 for (i=0;i<close_buttons.length;i++) {
     //var target_Id=close_buttons[i].getAttribute("data-target");
-    //close_buttons[i].onclick=close_Itself(target_Id);
+    //close_buttons[i].onclick=closeItself(target_Id);
     /*The above declaration has a bug,It will take effect immediately,but not after click.*/
     close_buttons[i].onclick=function () {
-        //close_Itself(target_Id);
-        close_Itself(this);
+        //closeItself(target_Id);
+        closeItself(this);
     };
 }
-function  close_Itself (close_button) {
+function  closeItself (close_button) {
     var target_id=close_button.getAttribute("data-target");
-    var target=document.querySelectorAll("#"+target_id);
-    target.style="display:none;"
+    var target=document.querySelector("#"+target_id);
+    target.style.display="none";
 }
