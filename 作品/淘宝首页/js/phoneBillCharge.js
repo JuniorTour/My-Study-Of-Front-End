827b5155d29cab5b914d669bb915dea249f57cc8
Junior-Tour <juniortour@qq.com>
2017-01-26 22:04:04 +0800

    2017年1月26日22:04:00

--- 作品/淘宝首页/js/phoneBillCharge.js
+++ 作品/淘宝首页/js/phoneBillCharge.js
@@ -12,28 +12,21 @@
  *
  * Third:click close and reveal box.
  */
-
-    /*First*/
-var convenientFunctionBtns=document.querySelectorAll(".convenient-function-btn");
-var i;
-for (i=0;i<4;i++) {
-    var currentCFBtn=convenientFunctionBtns[i];
-    var dataTarget=currentCFBtn.getAttribute("data-target");
-    (function (dataTarget) {
-        currentCFBtn.onmouseover=function () {
-            removeClass("active-convenient-function-btn");
-            addClass(this,"active-convenient-function-btn");
-            showFuncBox(dataTarget);
-            removeClass("active-phone-number-history");
-        }
-    })(dataTarget)
-}
-
-function removeClass(targetClass) {
+function removeOneClass(targetClass) {
     var targetEle=document.querySelector("."+targetClass);
     if (targetEle) {
         var classPattern=new RegExp(" "+targetClass);
         targetEle.className=targetEle.className.replace(classPattern,"");
+    }
+}
+
+function removeAllClass(targetClass) {
+    var targetEles=document.querySelectorAll("."+targetClass);
+    if (targetEles) {
+        var classPattern=new RegExp(" "+targetClass);
+        for (var i=0;i<targetEles.length;i++) {
+            targetEles[i].className=targetEles[i].className.replace(classPattern,"");
+        }
     }
 }
 
@@ -45,37 +38,97 @@ function addClass(targetEle,targetClass) {
 
 function showFuncBox(dataTarget) {
     var targetEle=document.querySelector("."+dataTarget);
-    removeClass("active-function-box");
+    removeOneClass("active-function-box");
     addClass(targetEle,"active-function-box");
     //targetEle.style.display="block";
+    /*reset value*/
+    fillInput("#phone-number","");
+    fillInput("#phone-face-value","30元");
+    changeCalculatedPrice("30");
 }
+
+    /*First*/
+var convenientFunctionBtns=document.querySelectorAll(".convenient-function-btn");
+var i;
+for (i=0;i<4;i++) {
+    var currentCFBtn=convenientFunctionBtns[i];
+    var dataTarget=currentCFBtn.getAttribute("data-target");
+    (function (dataTarget) {
+        currentCFBtn.onmouseover=function () {
+            removeOneClass("active-convenient-function-btn");
+            addClass(this,"active-convenient-function-btn");
+            showFuncBox(dataTarget);
+            removeOneClass("active-display-block");
+        }
+    })(dataTarget)
+}
+
 
 /*Second*/
 var numberHistoryBtns=document.querySelectorAll(".number-history-btn");
 for (i=0;i<numberHistoryBtns.length;i++) {
     numberHistoryBtns[i].onclick=function () {
         var sameLevelNumberHistory=document.querySelector(".phone-number-history-wrapper");
-        if (sameLevelNumberHistory.className.indexOf("active-phone-number-history")==-1) {
-            addClass(sameLevelNumberHistory,"active-phone-number-history");
+        if (sameLevelNumberHistory.className.indexOf("active-display-block")==-1) {
+            addClass(sameLevelNumberHistory,"active-display-block");
         } else {
-            removeClass("active-phone-number-history");
+            removeOneClass("active-display-block");
         }
     }
 }
 
-var faceValueOptions=document.querySelectorAll("");
-
 var phoneNumberHistories=document.querySelectorAll(".phone-number-history");
 for (i=0;i<phoneNumberHistories.length;i++) {
     phoneNumberHistories[i].onclick=function () {
-        fillInput(this.lastChild.nodeValue);
-        removeClass("active-phone-number-history");
+        fillInput("#phone-number",this.getAttribute("data-value"));
+        removeOneClass("active-display-block");
     }
 }
 
-function fillInput(targetContent) {
-    var phoneNumberInput=document.querySelector("#phone-number");
+var faceValueBtns=document.querySelectorAll(".face-value-btn");
+for (i=0;i<faceValueBtns.length;i++) {
+    faceValueBtns[i].onclick=function () {
+        var sameLevelNumberHistory=document.querySelector(".phone-face-value-wrapper");
+        if (sameLevelNumberHistory.className.indexOf("active-display-block")==-1) {
+            addClass(sameLevelNumberHistory,"active-display-block");
+        } else {
+            removeOneClass("active-display-block");
+        }
+    }
+}
+
+var faceValueOptions=document.querySelectorAll(".phone-face-value-option");
+for (i=0;i<faceValueOptions.length;i++) {
+    faceValueOptions[i].onclick=function () {
+        fillInput("#phone-face-value",(this.getAttribute("data-value"))+"元");
+        changeCalculatedPrice(this.getAttribute("data-value"));
+        removeOneClass("active-display-block");
+    }
+}
+
+function fillInput(targetInputId,targetContent) {
+    var phoneNumberInput=document.querySelector(targetInputId);
     phoneNumberInput.value=targetContent;
+}
+
+function changeCalculatedPrice(dataValuePrice) {
+    var calculatedPrice;
+    switch (dataValuePrice) {
+        case "30":
+            calculatedPrice="29.4-30";
+            break;
+        case "50":
+            calculatedPrice="49-48";
+            break;
+        case "100":
+            calculatedPrice="98-99.5";
+            break;
+        case "150":
+            calculatedPrice="147-149";
+            break;
+    }
+    var calculatedPriceContent=document.querySelector(".phone-calculated-price");
+    calculatedPriceContent.innerHTML=calculatedPrice;
 }
 
 /*Third*/
@@ -83,8 +136,8 @@ var convenientCloseBtns=document.querySelectorAll(".convenient-box-close");
 
 for (i=0;i<convenientCloseBtns.length;i++) {
     convenientCloseBtns[i].onclick=function () {
-        removeClass("active-convenient-function-btn");
-        removeClass("active-function-box");
-        removeClass("active-phone-number-history");
+        removeOneClass("active-convenient-function-btn");
+        removeOneClass("active-function-box");
+        removeAllClass("active-display-block");
     }
 }
