65080819e61615226c67fe364a4ea6ac8e6f0dc1
Junior-Tour <juniortour@qq.com>
2017-05-14 12:25:06 +0800

    2017年5月14日12:25:05

--- 学过的课程/vue-guide/demos/shopping-cart-Vue.js/js/cart.js
+++ 学过的课程/vue-guide/demos/shopping-cart-Vue.js/js/cart.js
@@ -6,6 +6,7 @@ var app=new Vue({
     data: {
         totalMoney:0,
         productList:[],
+        checkAllFlag:false
     },
     filters: {
         formatMoney: function (val) {
@@ -25,7 +26,7 @@ var app=new Vue({
     },
     methods:{
         cartView:function () {
-            let _this=this;
+            //let _this=this;
             /*resource插件绑定在vue实例上，需要这样使用：*/
             this.$http.get('data/cartData.json',{id:123}).then(res=>{
                 this.totalMoney=res.body.result.totalMoney;
@@ -53,6 +54,36 @@ var app=new Vue({
                 * 此处的this指向的是这个vue实例，app。*/
                 item.checked=!item.checked;
             }
+            //如果所有的item都已选中或取消，则相应的更新checkAllFlag。
+            var checkAll=false;
+            for (var oneItem of this.productList) {
+                if (oneItem.checked===false || oneItem.checked===undefined) {
+                    checkAll=false;
+                    break;
+                } else {
+                    checkAll=true;
+                }
+            }
+            checkAll?this.checkAllFlag=true:this.checkAllFlag=false;
+        },
+        checkAll:function () {
+            this.checkAllFlag=!this.checkAllFlag;
+            if (this.checkAllFlag) {
+                //if check all:
+                this.productList.forEach((val,index)=> {
+                    Vue.set(val,'checked',true);
+                });
+            } else {
+                //if UN-check all:
+                this.productList.forEach((val,index)=> {
+                    Vue.set(val,'checked',false);
+                });
+            }
+
+            //this.productList.forEach((val,index)=> {
+            //    val.checked=val.checked?false:true;
+            //    this.checkItem(val);
+            //});
         }
     }
 });
