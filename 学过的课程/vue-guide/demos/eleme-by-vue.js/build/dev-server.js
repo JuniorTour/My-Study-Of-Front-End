efec8cf8bb4e54dbb30f27544e4dbd6280a67c6d
Junior-Tour <juniortour@qq.com>
2017-05-22 15:00:00 +0800

    学习webpack

--- 学过的课程/vue-guide/demos/eleme-by-vue.js/build/dev-server.js
+++ 学过的课程/vue-guide/demos/eleme-by-vue.js/build/dev-server.js
@@ -56,7 +56,7 @@ var devMiddleware = require('webpack-dev-middleware')(compiler, {
 })
 
 var hotMiddleware = require('webpack-hot-middleware')(compiler)
-// force page reload when html-webpack-plugin template changes
+// force page reload when html-w  ebpack-plugin template changes
 compiler.plugin('compilation', function (compilation) {
 	compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
 		hotMiddleware.publish({action: 'reload'})
